31f42c5cd1243b18ef23fcc5e821e4d3c024ab8d
Sean Sundberg <seansund@us.ibm.com>
2021-09-21 15:17:37 -0500

    Updates chart name
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/create-yaml.sh
+++ scripts/create-yaml.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
 MODULE_DIR=$(cd "${SCRIPT_DIR}/.."; pwd -P)
-CHART_DIR=$(cd "${MODULE_DIR}/chart/buildah-unprivileged"; pwd -P)
+CHART_DIR=$(cd "${MODULE_DIR}/chart/ocp-userspaces"; pwd -P)
 
 NAME="$1"
 DEST_DIR="$2"
