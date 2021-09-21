acc4e1b45fb16eec905ad4f653a6adaf760d37cf
Sean Sundberg <seansund@us.ibm.com>
2021-09-21 15:03:32 -0500

    Create dest dir
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/create-yaml.sh
+++ scripts/create-yaml.sh
@@ -7,6 +7,8 @@ CHART_DIR=$(cd "${MODULE_DIR}/chart/buildah-unprivileged"; pwd -P)
 NAME="$1"
 DEST_DIR="$2"
 
+mkdir -p "${DEST_DIR}"
+
 ## Add logic here to put the yaml resource content in DEST_DIR
 cp -R ${CHART_DIR}/* "${DEST_DIR}"
 
